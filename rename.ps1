a84d33be88bb03af98cd7fc07b99cea650a2a797
Sergio <sergio.passos88@gmail.com>
2018-04-01 14:43:07 -0300

    Organização de scripts

--- rename.ps1
+++
@@ -1,5 +0,0 @@
-Get-ChildItem -Filter "*.Table.sql" |Rename-Item -NewName {$_.name -replace '.Table.sql','.sql'}
-Get-ChildItem -Filter "*.UserDefinedFunction.sql" |Rename-Item -NewName {$_.name -replace '.UserDefinedFunction.sql','.sql'}
-Get-ChildItem -Filter "*.StoredProcedure.sql" |Rename-Item -NewName {$_.name -replace '.StoredProcedure.sql','.sql'}
-Get-ChildItem -Filter "*.View.sql" |Rename-Item -NewName {$_.name -replace '.View.sql','.sql'}
-Get-ChildItem -Filter "*.sql" |Rename-Item -NewName {$_.name -replace 'dbo.',''}
